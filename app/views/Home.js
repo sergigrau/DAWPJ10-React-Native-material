3c95d68e0b22bd0cec86300d3e8039934febeef5
sergi grau <sergi.grau@fje.edu>
2020-03-23 19:46:03 +0100

    actualització fins a props i states

--- app/views/Home.js
+++ app/views/Home.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-
+import {Login} from '../widget/Login'
 /**
  * Classe que hereta de Component i que implementa un component
  * independent en l'app. És un component bàsic sense estils
- * @version 1.0 sergi.grau@fje.edu
+ * @version 1.0 23.03.2020
+ * @author sergi.grau@fje.edu
  */
 export class Home extends React.Component {
     render() {
         return (
             <View >
-              <Text>Component bàsic</Text>
+              <Login missatge='polsar per a Login'></Login>
+              <Text>Aprenent React Native a DAW2</Text>
             </View>
           );
 
---
+++ app/widget/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+/**
+ * Classe que hereta de Component i que permet utilitzar 
+ * propietats inmutables
+ * @version 1.0 23.03.2020
+ * @author sergi.grau@fje.edu
+ */
+export class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={logat:false};
+    }
+    canviarLog = () =>{
+        if(this.state.logat){
+            this.setState({logat:false})
+        }
+        else {
+            this.setState({logat:true})
+        }
+    }
+    render() {
+
+        let missatge = this.state.logat?'Usuari anònim':this.props.missatge; //fem servir l'estat per a mostrar un o un altre missatge
+        return (
+            <View >
+                <Text onPress={this.canviarLog} >{missatge}</Text>
+            </View>
+        );
+
+    }
+}
