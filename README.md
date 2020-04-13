e624574407ae7f9469b3df4159d8082f16acfe7a
sergi grau <sergi.grau@fje.edu>
2020-04-13 19:37:19 +0200

    correcció SQLite

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DAW2M14-React-Native
 Workshop of react native
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/200px-React-icon.svg.png">
+
+M00. Simple React component
+M01. Properties and states
+M02. Styles
+M03. Events
+M04. Flex
+M05. Routing
+M06. Routing and components
+M07. Camera
+M08. Maps
+M09. SQLite
--- app/views/M09_Sqlite.js
+++ app/views/M09_Sqlite.js
@@ -1,74 +1,58 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import * as SQLite from 'expo-sqlite';
-import { openDatabase } from "expo-sqlite";
-
-import { TouchableHighlight, StyleSheet, Image, Text, View } from 'react-native';
-
 
 /**
- * Classe que hereta de Component i que implementa un accés simple a la base de dades
- * SQLite , fa servir routing
- * @version 1.0 05.04.2020
+ * Classe que hereta de Component i que treballa amb estils
+ * @version 1.0 23.03.2020
  * @author sergi.grau@fje.edu
  */
 
-
-
-const styles = StyleSheet.create({
-
+const estils = StyleSheet.create({
+  textPeu: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  peu: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#FA0'
+  },
 });
 
 export class M09_Sqlite extends React.Component {
-
   constructor(props) {
     super(props);
-    db.transaction(function(txn) {
-      txn.executeSql(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
-        [],
-        function(tx, res) {
-          console.log('item:', res.rows.length);
-          if (res.rows.length == 0) {
-            txn.executeSql('DROP TABLE IF EXISTS table_user', []);
-            txn.executeSql(
-              'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_contact INT(10), user_address VARCHAR(255))',
-              []
-            );
-          }
-        }
+    this.state = {
+      path: null,
+    };
+    db = SQLite.openDatabase("db.db");
+
+    db.transaction(tx => {
+      tx.executeSql(
+        "create table if not exists items (id integer primary key not null, done int, value text);"
       );
     });
+    console.log('creada taula');
+    db.transaction(
+      tx => {
+        tx.executeSql("insert into items (done, value) values (0, ?)", ['primer']);
+        tx.executeSql("insert into items (done, value) values (1, ?)", ['segon']);
+        tx.executeSql("insert into items (done, value) values (2, ?)", ['tercer']);
+        tx.executeSql("select * from items", [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      }
+    );
   }
+
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'white',
-          flexDirection: 'column',
-        }}>
-        <Mytext text="SQLite Example" />
-        <Mybutton
-          title="Register"
-          customClick={() => this.props.navigation.navigate('Register')}
-        />
-        <Mybutton
-          title="Update"
-          customClick={() => this.props.navigation.navigate('Update')}
-        />
-        <Mybutton
-          title="View"
-          customClick={() => this.props.navigation.navigate('View')}
-        />
-        <Mybutton
-          title="View All"
-          customClick={() => this.props.navigation.navigate('ViewAll')}
-        />
-        <Mybutton
-          title="Delete"
-          customClick={() => this.props.navigation.navigate('Delete')}
-        />
+      <View style={estils.peu}>
+        <Text style={estils.textPeu}> SQLITE </Text>
       </View>
     );
+
   }
 }
