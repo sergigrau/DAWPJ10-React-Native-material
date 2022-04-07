1278e0d06325139f83493183b494bb7c15d81dbd
sergi grau <sergi.grau@fje.edu>
2022-04-07 15:02:20 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import {M00_Home} from './app/views/M00_Home';
 
 import {M06_Home} from './app/views/M06_Home_routing';
 import {M06_Detalls} from './app/views/M06_Detalls_routing';
 import {M07_Camera} from './app/views/M07_Camera';
-import {M08_Mapes} from './app/views/M08_Mapes';
 import {M09_Sqlite} from './app/views/M09_Sqlite';
+import { M08_Mapes } from './app/views/M08_Mapes';
+import { M10_Suma } from './app/views/M10_Suma';
 /**
  * Modificacions al component principal d'entrada de React
  * per incloure encaminaments, però no components
@@ -17,14 +19,18 @@ import {M09_Sqlite} from './app/views/M09_Sqlite';
 const Stack = createStackNavigator();
 
 function App() {
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
+       <Stack.Screen name="Zero" component={M00_Home} />
         <Stack.Screen name="Home" component={M06_Home} />
         <Stack.Screen name="Detall" component={M06_Detalls} />
         <Stack.Screen name="Camera" component={M07_Camera} />
         <Stack.Screen name="Mapes" component={M08_Mapes} />
         <Stack.Screen name="SQLite" component={M09_Sqlite} />
+        <Stack.Screen name="Suma" component={M10_Suma} />
       </Stack.Navigator>
     </NavigationContainer>
   );
