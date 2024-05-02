848072c947d39276f9da0860fb1d9acd6b368b66
sergi grau <sergi.grau@fje.edu>
2024-05-02 19:08:46 +0200

    Add files via upload

--- app/views/M07_Camera.js
+++ app/views/M07_Camera.js
@@ -1,111 +1,51 @@
 import React from 'react';
-import { TouchableHighlight, StyleSheet, Image, Text, View } from 'react-native';
-
-
-/**
- * Classe que hereta de Component i que implementa un component
- * independent en l'app. És un component bàsic sense estils
- * Fa servir routing
- * @version 1.0 05.04.2020
- * @author sergi.grau@fje.edu
- */
-
+import { View, Button } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#000',
-  },
-  preview: {
-    flex: 2,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    
-  },
-  capture: {
-    width: 200,
-    height: 40,
-    borderRadius: 35,
-    borderWidth: 5,
-    borderColor: '#FFF',
-    marginBottom: 15,
-
-  },
-  cancel: {
-    position: 'absolute',
-    right: 20,
-    top: 20,
-    backgroundColor: 'transparent',
-    color: '#FFF',
-    fontWeight: '600',
-    fontSize: 17,
-  }
-});
 
 export class M07_Camera extends React.Component {
+  cameraRef = React.createRef();
+
   constructor(props) {
     super(props);
     this.state = {
-      path: null,
+      hasPermission: null,
     };
   }
-  ferFoto = async () => {
-    try {
-      const data = await this.camera.takePictureAsync();
-      this.setState({ path: data.uri });
-      // this.props.updateImage(data.uri);
-      // console.log('Path to image: ' + data.uri);
-    } catch (err) {
-      console.log('err: ', err);
+
+  async componentDidMount() {
+    const { status } = await Camera.requestPermissionsAsync();
+    this.setState({ hasPermission: status === 'granted' });
+    const { status: mediaLibStatus } = await MediaLibrary.requestPermissionsAsync();
+  this.setState({ hasMediaLibPermission: mediaLibStatus === 'granted' });
+  }
+
+  takePicture = async () => {
+    if (this.cameraRef.current) {
+      let photo = await this.cameraRef.current.takePictureAsync();
+      console.log(photo.uri);
+      const asset = await MediaLibrary.createAssetAsync(photo.uri);
+      await MediaLibrary.createAlbumAsync('DAW2', asset, false);
     }
   };
-  renderCamera() {
-    return (
-      <Camera
-        ref={(cam) => {
-          this.camera = cam;
-        }}
-        style={styles.preview}
-        flashMode={Camera.Constants.FlashMode.off}
-        permissionDialogTitle={'permis denegat'}
-        permissionDialogMessage={'calen permisos per a fer servir la càmera'}
-      >
-        <TouchableHighlight
-          style={styles.capture}
-          onPress={this.ferFoto.bind(this)}
-          underlayColor="rgba(255, 255, 255, 0.5)"
-        >
-          <View />
-        </TouchableHighlight>
-      </Camera>
-    );
-  }
-
-  renderImage() {
-    return (
-      <View>
-        <Image
-          source={{ uri: this.state.path }}
-          style={styles.preview}
-        />
-        <Text
-          style={styles.cancel}
-          onPress={() => this.setState({ path: null })}
-        >Cancel
-        </Text>
-      </View>
-    );
-  }
 
   render() {
+    const { hasPermission } = this.state;
+
+    if (hasPermission === null) {
+      return <View />;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+
     return (
-      <View style={styles.container}>
-        {this.state.path ? this.renderImage() : this.renderCamera()}
+      <View style={{ flex: 1 }}>
+        <Camera style={{ flex: 1 }} ref={this.cameraRef} />
+        <Button title="Fes una foto" onPress={this.takePicture} />
       </View>
     );
   }
 }
+
