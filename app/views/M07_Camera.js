ad88bd3e5733475832f6272f4b5122efaabdb374
sergi grau <sergi.grau@fje.edu>
2024-05-06 09:06:59 +0200

    Add files via upload

--- app/views/M07_Camera.js
+++ app/views/M07_Camera.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 
@@ -18,15 +18,22 @@ export class M07_Camera extends React.Component {
     const { status } = await Camera.requestPermissionsAsync();
     this.setState({ hasPermission: status === 'granted' });
     const { status: mediaLibStatus } = await MediaLibrary.requestPermissionsAsync();
-  this.setState({ hasMediaLibPermission: mediaLibStatus === 'granted' });
-  }
+    this.setState({ hasMediaLibPermission: mediaLibStatus === 'granted' });
 
-  takePicture = async () => {
-    if (this.cameraRef.current) {
-      let photo = await this.cameraRef.current.takePictureAsync();
-      console.log(photo.uri);
-      const asset = await MediaLibrary.createAssetAsync(photo.uri);
-      await MediaLibrary.createAlbumAsync('DAW2', asset, false);
+    if (mediaLibStatus === 'granted') {
+      const { assets } = await MediaLibrary.getAssetsAsync({ first: 1 });
+      if (assets.length > 0) {
+        this.setState({ imageUri: assets[0].uri });
+      }
+    }
+
+    ferFoto = async () => {
+      if (this.cameraRef.current) {
+        let photo = await this.cameraRef.current.takePictureAsync();
+        console.log(photo.uri);
+        const asset = await MediaLibrary.createAssetAsync(photo.uri);
+        await MediaLibrary.createAlbumAsync('DAW2', asset, false);
+      }
     }
   };
 
@@ -43,7 +50,11 @@ export class M07_Camera extends React.Component {
     return (
       <View style={{ flex: 1 }}>
         <Camera style={{ flex: 1 }} ref={this.cameraRef} />
-        <Button title="Fes una foto" onPress={this.takePicture} />
+        <Button title="Fes una foto" onPress={this.ferFoto} />
+        <Image source={require('../../assets/favicon.png')} />
+        <View>
+          {this.state.imageUri && <Image source={{ uri: this.state.imageUri }} style={{ width: 200, height: 200 }} />}
+        </View>
       </View>
     );
   }
